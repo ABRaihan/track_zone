d64b82c315572ff7573e3c5540e8a72df165445e
ABRaihan <abraihan7244@gmail.com>
2023-03-18 00:42:59 +0600

    format: environment setup done

---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Inputs from "../pages/Inputs";
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Inputs />
+    </>
+  );
+}
